69c236b4c650da15ac5a4cb8ed53a2062f99e0ac
Esra Yılmaz <yilmazeesraa@gmail.com>
2026-09-21 22:52:24 +0300

    Bilgiler classi eklendi

---
+++ src/Bilgiler.java
@@ -0,0 +1,12 @@
+public class Bilgiler {
+    public static void main(String[] args) {
+        String isim="Esra";
+        int yas=27;
+        double boy= 1.68;
+        System.out.println("Adim:"+isim);
+        System.out.println("Yasim:"+yas);
+        System.out.println("Boyum:"+boy);
+
+
+    }
+}
